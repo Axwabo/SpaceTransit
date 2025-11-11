f44c83f3901391ba356e44d30ae3fac96e81eafa
Axwabo <axwabo@gmail.com>
2025-11-11 21:59:28 +0100

    begin tube system

---
+++ Assets/Scripts/SpaceTransit/Tubes/SplineTube.cs
@@ -0,0 +1,29 @@
+﻿using SplineMesh;
+using UnityEngine;
+
+namespace SpaceTransit.Tubes
+{
+
+    public sealed class SplineTube : TubeBase
+    {
+
+        [SerializeField]
+        private Spline spline;
+
+        [SerializeField]
+        private float length;
+
+        public override float Length => length <= 0 ? spline.Length : length;
+
+        public override (Vector3, Quaternion) Sample(float distance)
+        {
+            var sample = spline.GetSampleAtDistance(distance);
+            return (sample.location, sample.Rotation);
+        }
+
+        public override float GetDistance(Vector3 point)
+            => spline.GetProjectionSample(point).timeInCurve * Length;
+
+    }
+
+}
---
+++ Assets/Scripts/SpaceTransit/Tubes/TubeBase.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace SpaceTransit.Tubes
+{
+
+    public abstract class TubeBase : MonoBehaviour
+    {
+
+        public abstract float Length { get; }
+
+        public abstract (Vector3, Quaternion) Sample(float distance);
+
+        public abstract float GetDistance(Vector3 point);
+
+    }
+
+}
