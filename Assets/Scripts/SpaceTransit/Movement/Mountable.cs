d474b7d15fb5c97f32ad33f7cfbd145a50e1099d
Axwabo <axwabo@gmail.com>
2025-11-12 16:13:10 +0100

    basic movement

---
+++ Assets/Scripts/SpaceTransit/Movement/Mountable.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace SpaceTransit.Movement
+{
+
+    public sealed class Mountable : MonoBehaviour
+    {
+
+        private Transform _t;
+
+        private void Awake() => _t = transform;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out MovementController controller))
+                controller.Mount = _t;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.TryGetComponent(out MovementController controller) && controller.Mount == _t)
+                controller.Mount = null;
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/SpaceTransit/Movement/MovementController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace SpaceTransit.Movement
+{
+
+    [RequireComponent(typeof(CharacterController))]
+    public sealed class MovementController : MonoBehaviour
+    {
+
+        public float speed;
+
+        public float gravity;
+
+        public float jumpVelocity;
+
+        private Transform _t;
+
+        private CharacterController _cc;
+
+        private Transform _mount;
+
+        private bool _isMounted;
+
+        private Vector3 _mountOffset;
+
+        private float _verticalVelocity;
+
+        public Transform Mount
+        {
+            get => _mount;
+            set => _isMounted = _t.parent = _mount = value;
+        }
+
+        private void Awake()
+        {
+            _t = transform;
+            _cc = GetComponent<CharacterController>();
+        }
+
+        private void Update()
+        {
+            if (_cc.isGrounded)
+                UpdateGrounded();
+            else
+                _verticalVelocity += gravity * Time.deltaTime;
+
+            var desiredMove = InputSystem.actions["Move"].ReadValue<Vector2>();
+            var move = _t.rotation * new Vector3(desiredMove.x, 0, desiredMove.y).normalized;
+            move.y = _verticalVelocity;
+            if (move != Vector3.zero)
+                _cc.Move(Time.deltaTime * speed * move);
+
+            if (_isMounted)
+                _mountOffset = _mount.InverseTransformPoint(_t.position);
+        }
+
+        private void LateUpdate()
+        {
+            if (_isMounted)
+                _t.position = _mount.TransformPoint(_mountOffset);
+        }
+
+        private void UpdateGrounded()
+        {
+            if (InputSystem.actions["Jump"].IsPressed())
+                _verticalVelocity = jumpVelocity;
+            else if (_verticalVelocity < 0)
+                _verticalVelocity = 0;
+        }
+
+    }
+
+}
