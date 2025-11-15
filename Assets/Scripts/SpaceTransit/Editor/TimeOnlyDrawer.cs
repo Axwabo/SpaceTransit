6628b9da49bbcfd08bd59ef53a4a2d8857a415a3
Axwabo <axwabo@gmail.com>
2025-11-15 18:27:13 +0100

    fix TimeOnlyDrawer

--- Assets/Scripts/SpaceTransit/Editor/TimeOnlyDrawer.cs
+++ Assets/Scripts/SpaceTransit/Editor/TimeOnlyDrawer.cs
@@ -6,15 +6,21 @@ namespace SpaceTransit.Editor
 {
 
     [CustomPropertyDrawer(typeof(TimeOnly))]
-    public class TimeOnlyDrawer : PropertyDrawer
+    public sealed class TimeOnlyDrawer : PropertyDrawer
     {
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             var time = (TimeOnly) property.boxedValue;
             var value = EditorGUI.TextField(position, label, $"{time.Value:hh':'mm}");
-            if (TimeSpan.TryParse(value, out var result))
-                time.Value = result;
+            if (!value.Contains(':'))
+                return;
+            var split = value.Split(':');
+            if (!int.TryParse(split[0], out var h) || !int.TryParse(split[1], out var m))
+                return;
+            var timeValue = new TimeSpan(h, m, 0);
+            if (timeValue != time.Value)
+                property.boxedValue = (TimeOnly) timeValue;
         }
 
     }
