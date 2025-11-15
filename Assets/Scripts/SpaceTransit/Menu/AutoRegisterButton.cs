6d81a44b3ef8cbb990dcc4959ff987da6d6de2ce
Axwabo <axwabo@gmail.com>
2025-11-15 11:22:28 +0100

    SyncSceneSwitcherButton

---
+++ Assets/Scripts/SpaceTransit/Menu/AutoRegisterButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SpaceTransit.Menu
+{
+
+    [RequireComponent(typeof(Button))]
+    public abstract class AutoRegisterButton : MonoBehaviour
+    {
+
+        private void Awake() => GetComponent<Button>().onClick.AddListener(Click);
+
+        protected abstract void Click();
+
+    }
+
+}
---
+++ Assets/Scripts/SpaceTransit/Menu/SyncSceneSwitcherButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SpaceTransit.Menu
+{
+
+    public sealed class SyncSceneSwitcherButton : AutoRegisterButton
+    {
+
+        [SerializeField]
+        private string sceneName;
+
+        protected override void Click() => SceneManager.LoadScene(sceneName);
+
+    }
+
+}
