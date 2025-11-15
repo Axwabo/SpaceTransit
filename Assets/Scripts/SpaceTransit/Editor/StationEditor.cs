ac841192b2e2ec4a2d180e1a64f83905bc50609c
Axwabo <axwabo@gmail.com>
2025-11-15 17:57:42 +0100

    StationEditor

---
+++ Assets/Scripts/SpaceTransit/Editor/StationEditor.cs
@@ -0,0 +1,33 @@
+﻿using SpaceTransit.Routes;
+using UnityEditor;
+using UnityEngine;
+
+namespace SpaceTransit.Editor
+{
+
+    [CustomEditor(typeof(Station))]
+    [CanEditMultipleObjects]
+    public sealed class StationEditor : UnityEditor.Editor
+    {
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            if (GUILayout.Button("Set Name from ID"))
+                Set();
+        }
+
+        private void Set()
+        {
+            Undo.RecordObjects(targets, "Set Name from ID");
+            foreach (var o in targets)
+            {
+                var station = (Station) o;
+                station.name = station.Name;
+                EditorUtility.SetDirty(o);
+            }
+        }
+
+    }
+
+}
