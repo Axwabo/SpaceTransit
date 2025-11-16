ee411c6b694be9207fda422e9a0bd2dbc8182296
Axwabo <axwabo@gmail.com>
2025-11-16 14:45:24 +0100

    add missing rotation extension

--- Assets/Scripts/SplineExtensions.cs
+++ Assets/Scripts/SplineExtensions.cs
@@ -7,4 +7,6 @@ public static class SplineExtensions
     public static CurveSample SampleNearest(this Spline spline, Vector3 position)
         => spline.GetSampleAtDistance(spline.GetProjectionSample(position).distanceInCurve);
 
+    public static Quaternion Rotation(this SplineNode node) => Quaternion.LookRotation(node.Direction - node.Position);
+
 }
