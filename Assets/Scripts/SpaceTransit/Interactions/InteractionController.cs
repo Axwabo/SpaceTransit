8e83fca7efb683538e86d53d9ccd133fd3786a17
Axwabo <axwabo@gmail.com>
2025-11-16 11:44:59 +0100

    use ignore raycast layer in FLASH + fix max interaction distance

--- Assets/Scripts/SpaceTransit/Interactions/InteractionController.cs
+++ Assets/Scripts/SpaceTransit/Interactions/InteractionController.cs
@@ -7,6 +7,8 @@ namespace SpaceTransit.Interactions
     public sealed class InteractionController : MonoBehaviour
     {
 
+        private const float MaxDistance = 3 * World.MetersToWorld;
+
         private Transform _t;
 
         private void Awake() => _t = transform;
@@ -14,7 +16,7 @@ namespace SpaceTransit.Interactions
         private void Update()
         {
             if (InputSystem.actions["Interact"].WasPressedThisFrame()
-                && Physics.Raycast(_t.position, _t.forward, out var hit, 3)
+                && Physics.Raycast(_t.position, _t.forward, out var hit, MaxDistance)
                 && hit.collider.TryGetComponent(out IInteractable interactable))
                 interactable.OnInteracted();
         }
