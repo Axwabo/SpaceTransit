ea7ea42f1579f31f28b7d12ab50e8d2d1c80a7d1
Axwabo <axwabo@gmail.com>
2025-11-16 07:36:53 +0100

    DoorAlarm flash

--- Assets/Scripts/SpaceTransit/Ships/Modules/Doors/DoorAlarm.cs
+++ Assets/Scripts/SpaceTransit/Ships/Modules/Doors/DoorAlarm.cs
@@ -13,18 +13,45 @@ namespace SpaceTransit.Ships.Modules.Doors
         private AudioSource source;
 
         [SerializeField]
+        private MeshRenderer meshRenderer;
+
+        [SerializeField]
         private AudioClip beep;
+
+        [SerializeField]
+        private Material active;
 
         [SerializeField]
         private float interval;
 
+        [SerializeField]
+        private bool oddOnly;
+
+        private Material _inactive;
+
         private float _remaining;
+
+        private int _count;
+
+        private void Awake() => _inactive = meshRenderer.sharedMaterial;
 
         private void Update()
         {
-            if ((_remaining -= Time.deltaTime) > 0 || !controller.AlarmActive)
+            if (!controller.AlarmActive)
+            {
+                if (_count != 0)
+                    meshRenderer.sharedMaterial = _inactive;
+                _remaining = 0;
+                _count = 0;
                 return;
-            source.PlayOneShot(beep);
+            }
+
+            if ((_remaining -= Time.deltaTime) > 0)
+                return;
+            var odd = ++_count % 2 == 1;
+            if (odd || !oddOnly)
+                source.PlayOneShot(beep);
+            meshRenderer.sharedMaterial = odd ? active : _inactive;
             _remaining = interval;
         }
 
