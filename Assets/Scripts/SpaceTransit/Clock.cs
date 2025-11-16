98d7cb88c0e0c95956dbfa5df9d566c15fcb5c75
Axwabo <axwabo@gmail.com>
2025-11-16 09:25:51 +0100

    clock to component + split Gyuardsaint-Cárpenter

--- Assets/Scripts/SpaceTransit/Clock.cs
+++ Assets/Scripts/SpaceTransit/Clock.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using TMPro;
 using UnityEngine;
 
 namespace SpaceTransit
 {
 
-    public static class Clock
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public sealed class Clock : MonoBehaviour
     {
 
         public static TimeSpan Now => new TimeSpan(7, 29, 50).Add(TimeSpan.FromSeconds(Time.timeAsDouble));
@@ -12,6 +14,12 @@ namespace SpaceTransit
         public static float Delta => Mathf.Min(0.3f, Time.unscaledDeltaTime) * Time.timeScale;
 
         public static float FixedDelta => Mathf.Min(0.3f, Time.fixedUnscaledDeltaTime) * Time.timeScale;
+
+        private TextMeshProUGUI _text;
+
+        private void Awake() => _text = GetComponent<TextMeshProUGUI>();
+
+        private void Update() => _text.text = Now.ToString("hh':'mm':'ss");
 
     }
 
