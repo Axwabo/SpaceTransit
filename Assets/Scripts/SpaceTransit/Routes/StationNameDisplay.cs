0fb2cbd039b2c59b71d954c98f8b1b79deda6d50
Axwabo <axwabo@gmail.com>
2025-11-17 00:18:59 +0100

    StationNameDisplay

---
+++ Assets/Scripts/SpaceTransit/Routes/StationNameDisplay.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace SpaceTransit.Routes
+{
+
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public sealed class StationNameDisplay : MonoBehaviour
+    {
+
+        private void Awake() => GetComponent<TextMeshProUGUI>().text = GetComponentInParent<Station>().Name;
+
+    }
+
+}
