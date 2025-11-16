2caaef7271e91e09016b849a152522dfef14ac36
Axwabo <axwabo@gmail.com>
2025-11-16 20:25:53 +0100

    DestinationDisplay

---
+++ Assets/Scripts/SpaceTransit/Ships/Modules/Displays/DestinationDisplay.cs
@@ -0,0 +1,20 @@
+﻿using SpaceTransit.Vaulter;
+using TMPro;
+using UnityEngine;
+
+namespace SpaceTransit.Ships.Modules.Displays
+{
+
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public sealed class DestinationDisplay : VaulterComponentBase
+    {
+
+        private TextMeshProUGUI _text;
+
+        protected override void Awake() => _text = GetComponent<TextMeshProUGUI>();
+
+        public override void OnRouteChanged() => _text.text = IsInService ? Parent.Route.Destination.Station.name : "";
+
+    }
+
+}
