be474a73cc85907d483b436cf27761de0a7f5121
Axwabo <axwabo@gmail.com>
2025-11-17 05:11:45 +0100

    TimeScaleInput

---
+++ Assets/Scripts/SpaceTransit/Menu/TimeScaleInput.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SpaceTransit.Menu
+{
+
+    public sealed class TimeScaleInput : MonoBehaviour
+    {
+
+        [SerializeField]
+        private TextMeshProUGUI text;
+
+        [SerializeField]
+        private Slider slider;
+
+        public void UpdateValue() => Time.timeScale = slider.value;
+
+        private void Update() => text.text = Time.timeScale.ToString("N");
+
+    }
+
+}
