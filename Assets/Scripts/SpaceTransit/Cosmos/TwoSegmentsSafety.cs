99fa09feee708c97d66bf8312270494b47f5c6e2
Axwabo <axwabo@gmail.com>
2025-11-17 01:19:29 +0100

    re-add TwoSegmentsSafety

---
+++ Assets/Scripts/SpaceTransit/Cosmos/TwoSegmentsSafety.cs
@@ -0,0 +1,24 @@
+﻿using SpaceTransit.Ships;
+
+namespace SpaceTransit.Cosmos
+{
+
+    public sealed class TwoSegmentsSafety : SafetyEnsurer
+    {
+
+        public override bool CanProceed(ShipAssembly assembly)
+        {
+            var tube = Tube;
+            if (assembly.Reverse ? !tube.HasPrevious : !tube.HasNext)
+                return true;
+            var next = tube.Next(assembly.Reverse);
+            if (!next.Safety.IsFreeFor(assembly))
+                return false;
+            if (assembly.Reverse ? !next.HasPrevious : !next.HasNext)
+                return true;
+            return next.Next(assembly.Reverse).Safety.IsFreeFor(assembly);
+        }
+
+    }
+
+}
