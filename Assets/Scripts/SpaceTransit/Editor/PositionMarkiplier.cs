ddc9fbb388c1ff097f3ff9bfbae08d8b975d55c2
Axwabo <axwabo@gmail.com>
2025-12-02 08:17:39 +0100

    position multiplier window

---
+++ Assets/Scripts/SpaceTransit/Editor/PositionMarkiplier.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace SpaceTransit.Editor
+{
+
+    public sealed class PositionMarkiplier : EditorWindow
+    {
+
+        [MenuItem("Window/SpaceTransit/Position Markiplier")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<PositionMarkiplier>();
+            window.titleContent = new GUIContent("Position Multiplier");
+            window.Show();
+        }
+
+        private float _multiplier = 1;
+
+        private void OnGUI()
+        {
+            _multiplier = EditorGUILayout.FloatField("Multiplier", _multiplier);
+            var selection = Selection.gameObjects;
+            if (selection.Length == 0 || !GUILayout.Button("Update"))
+                return;
+            foreach (var o in selection)
+                o.transform.localPosition *= _multiplier;
+        }
+
+    }
+
+}
