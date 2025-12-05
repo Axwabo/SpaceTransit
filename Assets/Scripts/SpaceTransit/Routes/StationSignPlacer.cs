59f939378e258839ee0ade5c7c7ccdc5e7ca33db
Axwabo <axwabo@gmail.com>
2025-12-05 19:54:12 +0100

    attempt StationSignPlacer fix

--- Assets/Scripts/SpaceTransit/Routes/StationSignPlacer.cs
+++ Assets/Scripts/SpaceTransit/Routes/StationSignPlacer.cs
@@ -1,5 +1,4 @@
 ﻿using SpaceTransit.Tubes;
-using Unity.Mathematics;
 using UnityEngine;
 
 namespace SpaceTransit.Routes
@@ -10,33 +9,66 @@ namespace SpaceTransit.Routes
     {
 
         [SerializeField]
-        private TubeBase forwards;
+        private TubeBase origin;
 
         [SerializeField]
-        private TubeBase backwards;
+        private bool backwards;
 
         [SerializeField]
-        private float distance = 1050;
+        private float distance = 1000;
 
         private void Start()
         {
             var station = GetComponent<Station>();
-            if (!forwards)
-                forwards = station.Docks[^1].Tube.Next;
-
-            var remaining = distance;
-            var tube = forwards;
-            while (remaining > 0)
+            if (!origin)
             {
-                if (remaining > tube.Length || !tube.HasNext)
+                origin = station.Docks[backwards ? ^1 : 0].Tube;
+                if (backwards ? !origin.HasPrevious : !origin.HasNext)
+                    return;
+                origin = backwards ? origin.Previous : origin.Next;
+            }
+
+            var (tube, remaining) = backwards ? GetPlacementBackwards() : GetPlacementForwards();
+            PlaceSign(tube, remaining);
+        }
+
+        private (TubeBase Tube, float Remaining) GetPlacementForwards()
+        {
+            var current = 0f;
+            var tube = origin;
+            while (current < distance * World.MetersToWorld && distance * World.MetersToWorld - current > tube.Length)
+            {
+                if (!tube.HasNext)
                     break;
-                remaining -= tube.Length;
+                current += tube.Length;
                 tube = tube.Next;
             }
 
+            return (tube, distance - current);
+        }
+
+        private (TubeBase Tube, float Remaining) GetPlacementBackwards()
+        {
+            var current = 0f;
+            var tube = origin;
+            while (current < distance * World.MetersToWorld && distance * World.MetersToWorld - current > tube.Length)
+            {
+                if (!tube.HasPrevious)
+                    break;
+                current += tube.Length;
+                tube = tube.Previous;
+            }
+
+            return (tube, distance - current);
+        }
+
+        private void PlaceSign(TubeBase tube, float remaining)
+        {
             var (position, rotation) = tube.Sample(Mathf.Clamp(remaining, 0, tube.Length));
-            Instantiate(World.StationSignPrefab, tube.Transform).SetLocalPositionAndRotation(position, rotation * quaternion.Euler(0, 180, 0));
-            // TODO backwards
+            var clone = Instantiate(World.StationSignPrefab, tube.Transform);
+            if (!backwards)
+                rotation *= Quaternion.Euler(0, 180, 0);
+            clone.SetLocalPositionAndRotation(position, rotation);
         }
 
     }
