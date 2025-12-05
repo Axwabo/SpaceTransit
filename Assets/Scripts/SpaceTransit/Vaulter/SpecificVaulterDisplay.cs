201ccd223f45cae3e7948d72c9000046af29e222
Axwabo <axwabo@gmail.com>
2025-12-05 21:04:26 +0100

    begin vaulter display system

---
+++ Assets/Scripts/SpaceTransit/Vaulter/SpecificVaulterDisplay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SpaceTransit.Vaulter
+{
+
+    [RequireComponent(typeof(RawImage))]
+    public sealed class SpecificVaulterDisplay : MonoBehaviour
+    {
+
+        [SerializeField]
+        private VaulterRenderer vaulterRenderer;
+
+        private void Start() => GetComponent<RawImage>().texture = vaulterRenderer.Texture;
+
+    }
+
+}
---
+++ Assets/Scripts/SpaceTransit/Vaulter/VaulterRenderer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace SpaceTransit.Vaulter
+{
+
+    public sealed class VaulterRenderer : MonoBehaviour
+    {
+
+        [SerializeField]
+        private int width;
+
+        [SerializeField]
+        private int height;
+
+        [SerializeField]
+        private Canvas canvas;
+
+        [SerializeField]
+        private Camera cam;
+
+        public RenderTexture Texture { get; private set; }
+
+        private void Awake()
+        {
+            Texture = RenderTexture.GetTemporary(width, height);
+            cam.targetTexture = Texture;
+            canvas.worldCamera = cam;
+            transform.parent = null;
+        }
+
+        private void OnDestroy() => RenderTexture.ReleaseTemporary(Texture);
+
+    }
+
+}
