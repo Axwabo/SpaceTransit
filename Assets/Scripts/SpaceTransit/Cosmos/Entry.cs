18a7206190180bc0ab7483c74ad4aff3aaa9d248
Axwabo <axwabo@gmail.com>
2025-12-06 10:13:47 +0100

    Revert "remove exit use when clearing entry"
    This reverts commit 0a15b2f11a14bdaac677d7c049c9102d23bc986f.

--- Assets/Scripts/SpaceTransit/Cosmos/Entry.cs
+++ Assets/Scripts/SpaceTransit/Cosmos/Entry.cs
@@ -39,12 +39,6 @@ namespace SpaceTransit.Cosmos
 
         private void Update() => UsedBy.RemoveWhere(e => !e);
 
-        public void Clear(ShipAssembly assembly)
-        {
-            UsedBy.Remove(assembly);
-            exit.UsedBy.Remove(assembly);
-        }
-
     }
 
 }
