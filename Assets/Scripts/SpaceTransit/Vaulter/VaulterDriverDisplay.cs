77c13789484db278373acdadd6c0ffade65c84f2
Axwabo <axwabo@gmail.com>
2025-12-06 18:44:15 +0100

    rename DepartureTimeDisplay & more info

---
+++ Assets/Scripts/SpaceTransit/Vaulter/VaulterDriverDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using SpaceTransit.Routes.Stops;
+using TMPro;
+using UnityEngine;
+
+namespace SpaceTransit.Vaulter
+{
+
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public sealed class VaulterDriverDisplay : VaulterComponentBase
+    {
+
+        private static readonly StringBuilder Builder = new();
+
+        private TextMeshProUGUI _text;
+
+        protected override void Awake() => _text = GetComponent<TextMeshProUGUI>();
+
+        public override void OnStopChanged()
+        {
+            Builder.Clear().AppendLine(Parent.Stop.Station.name);
+            if (Parent.Stop is IArrival {Arrival: var arrival})
+                Builder.AppendLine($"Arrive at {arrival.Value:hh':'mm}");
+            if (Parent.Stop is IDeparture {Departure: var time})
+                Builder.AppendLine($"Depart at {time.Value:hh':'mm}");
+            _text.text = Builder.Append("Dock: ").Append(Parent.Stop.DockIndex + 1).ToString();
+        }
+
+    }
+
+}
