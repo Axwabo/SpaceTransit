e79a1bc58e310184ae52b58f7ac00573cf3dbfb0
Axwabo <axwabo@gmail.com>
2025-12-07 18:08:23 +0100

    ROSS liftoff sound

--- Assets/Scripts/SpaceTransit/Ships/Modules/VisualThruster.cs
+++ Assets/Scripts/SpaceTransit/Ships/Modules/VisualThruster.cs
@@ -9,16 +9,28 @@ namespace SpaceTransit.Ships.Modules
 
         private AudioSource _source;
 
+        private bool _swap;
+
+        [SerializeField]
+        private AudioClip liftoff;
+
+        [SerializeField]
+        private AudioClip land;
+
         protected override void Awake()
         {
             base.Awake();
             _source = GetComponent<AudioSource>();
+            _swap = liftoff != land;
         }
 
         public override void OnStateChanged()
         {
-            if (State is ShipState.LiftingOff or ShipState.Landing)
-                _source.Play();
+            if (State is not (ShipState.LiftingOff or ShipState.Landing))
+                return;
+            if (_swap)
+                _source.clip = State == ShipState.LiftingOff ? liftoff : land;
+            _source.Play();
         }
 
     }
