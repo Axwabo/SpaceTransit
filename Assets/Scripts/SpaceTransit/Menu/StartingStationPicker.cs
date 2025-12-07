28412e2667bf616e674fe0abfa3128f7634623f7
Axwabo <axwabo@gmail.com>
2025-12-07 22:47:53 +0100

    move resources for now + prepare scene

--- Assets/Scripts/SpaceTransit/Menu/StartingStationPicker.cs
+++ Assets/Scripts/SpaceTransit/Menu/StartingStationPicker.cs
@@ -17,10 +17,12 @@ namespace SpaceTransit.Menu
         private void Start()
         {
             var list = Cache.Stations.Select(static e => e.name).ToList();
+            var index = list.IndexOf(defaultValue);
             var dropdown = GetComponent<TMP_Dropdown>();
             dropdown.AddOptions(list);
-            dropdown.value = Mathf.Max(0, list.IndexOf(defaultValue));
+            dropdown.value = Mathf.Max(0, index);
             dropdown.onValueChanged.AddListener(Change);
+            Change(index);
         }
 
         private static void Change(int arg0) => MovementController.StartingStation = Cache.Stations[arg0];
