24ca4722fd17e176ae0ec493db4386b2c8942b54
Axwabo <axwabo@gmail.com>
2025-12-07 10:35:11 +0100

    arrivals board

---
+++ Assets/Scripts/SpaceTransit/Stations/ArrivalDisplay.cs
@@ -0,0 +1,18 @@
+﻿using SpaceTransit.Vaulter;
+
+namespace SpaceTransit.Stations
+{
+
+    public sealed class ArrivalDisplay : EntryDisplayBase<ArrivalEntry>
+    {
+
+        public override void Apply(ArrivalEntry item) => Apply(
+            item.Route.Type,
+            item.Route.Destination.Station.name,
+            item.Arrival.Arrival.Value,
+            item.Arrival.DockIndex
+        );
+
+    }
+
+}
---
+++ Assets/Scripts/SpaceTransit/Stations/ArrivalsBoard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using SpaceTransit.Vaulter;
+
+namespace SpaceTransit.Stations
+{
+
+    public sealed class ArrivalsBoard : Board<ArrivalEntry, ArrivalDisplay>
+    {
+
+        protected override IReadOnlyCollection<ArrivalEntry> GetSource(DeparturesArrivals departuresArrivals) => departuresArrivals.Arrivals;
+
+        protected override TimeOnly GetTime(ArrivalEntry entry) => entry.Arrival.Arrival;
+
+    }
+
+}
