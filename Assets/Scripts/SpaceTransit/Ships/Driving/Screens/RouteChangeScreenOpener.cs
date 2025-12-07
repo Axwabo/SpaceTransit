47d4406bbee2aa90419e7334b616acb1e0d61be2
Axwabo <axwabo@gmail.com>
2025-12-07 22:53:33 +0100

    screen button icons

---
+++ Assets/Scripts/SpaceTransit/Ships/Driving/Screens/RouteChangeScreenOpener.cs
@@ -0,0 +1,17 @@
+﻿using SpaceTransit.Vaulter;
+using UnityEngine;
+
+namespace SpaceTransit.Ships.Driving.Screens
+{
+
+    public sealed class RouteChangeScreenOpener : VaulterComponentBase
+    {
+
+        [SerializeField]
+        private ScreenSlot slot;
+
+        public override void OnRouteChanged() => slot.Show(ScreenSlot.Slot.Exits);
+
+    }
+
+}
