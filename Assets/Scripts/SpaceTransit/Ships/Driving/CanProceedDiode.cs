f3513b1c33a859ec283bba7d980c0cdf35674007
Axwabo <axwabo@gmail.com>
2025-12-07 22:27:30 +0100

    CanProceedDiode

---
+++ Assets/Scripts/SpaceTransit/Ships/Driving/CanProceedDiode.cs
@@ -0,0 +1,20 @@
+﻿using SpaceTransit.Ships.Modules;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SpaceTransit.Ships.Driving
+{
+
+    [RequireComponent(typeof(Image))]
+    public sealed class CanProceedDiode : ModuleComponentBase
+    {
+
+        private Image _image;
+
+        protected override void Awake() => _image = GetComponent<Image>();
+
+        private void Update() => _image.color = Controller.CanProceed ? Color.green : Color.red;
+
+    }
+
+}
