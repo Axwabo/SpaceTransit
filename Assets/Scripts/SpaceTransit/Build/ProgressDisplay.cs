49109cee73948a35560a36153affdd41e8a01829
Axwabo <axwabo@gmail.com>
2025-12-10 21:27:46 +0100

    ProgressDisplay

---
+++ Assets/Scripts/SpaceTransit/Build/ProgressDisplay.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace SpaceTransit.Build
+{
+
+    public sealed class ProgressDisplay : MonoBehaviour
+    {
+
+        [SerializeField]
+        private TextMeshProUGUI text;
+
+        [SerializeField]
+        private RectTransform rect;
+
+        private void Update()
+        {
+            text.text = $"Loading... {GradualTubeLoader.Progress:P0}";
+            rect.localScale = new Vector3(GradualTubeLoader.Progress, 1);
+        }
+
+    }
+
+}
