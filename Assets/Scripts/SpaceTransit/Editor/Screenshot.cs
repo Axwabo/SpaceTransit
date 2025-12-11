9a169cc1b96438c3de86a4e9ddc0a9c487cfac83
Axwabo <axwabo@gmail.com>
2025-12-11 16:13:06 +0100

    screenshot taker

---
+++ Assets/Scripts/SpaceTransit/Editor/Screenshot.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace SpaceTransit.Editor
+{
+
+    public static class Screenshot
+    {
+
+        [MenuItem("Assets/Screenshot")]
+        public static void Take() => ScreenCapture.CaptureScreenshot("Assets/Screenshot.png");
+
+    }
+
+}
