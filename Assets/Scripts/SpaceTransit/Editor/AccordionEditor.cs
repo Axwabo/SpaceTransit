19f5b244a2ae885d843d4662dca62a63ae67b9fd
Axwabo <axwabo@gmail.com>
2025-12-11 15:21:51 +0100

    fix closest vertex index finding

--- Assets/Scripts/SpaceTransit/Editor/AccordionEditor.cs
+++ Assets/Scripts/SpaceTransit/Editor/AccordionEditor.cs
@@ -31,38 +31,28 @@ namespace SpaceTransit.Editor
         {
             if (target is not Accordion accordion)
                 return;
-            var ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
-            Raycast(ray, accordion.from, ref _from);
-            Raycast(ray, accordion.to, ref _to);
+            FindClosest(accordion.from, ref _from);
+            FindClosest(accordion.to, ref _to);
             if (_applyFrom)
                 accordion.fromVertices[^1] = _from;
             if (_applyTo)
                 accordion.toVertices[^1] = _to;
         }
 
-        private static void Raycast(Ray ray, MeshFilter from, ref int index)
+        private static void FindClosest(MeshFilter from, ref int index)
         {
-            if (!from.TryGetComponent(out MeshCollider collider)
-                || !collider.Raycast(ray, out var hit, 50))
-                return;
             var vertices = from.sharedMesh.vertices;
-            var triangles = from.sharedMesh.triangles;
-            var t = collider.transform;
-            var ia = triangles[hit.triangleIndex];
-            var ib = triangles[hit.triangleIndex] + 1;
-            var ic = triangles[hit.triangleIndex] + 2;
-            var a = t.TransformPoint(vertices[ia]);
-            var b = t.TransformPoint(vertices[ib]);
-            var c = t.TransformPoint(vertices[ic]);
-            var da = Vector3.Distance(hit.point, a);
-            var db = Vector3.Distance(hit.point, b);
-            var dc = Vector3.Distance(hit.point, c);
-            index = ia;
-            var minDistance = da;
-            if (db < minDistance)
-                (index, minDistance) = (ib, db);
-            if (dc < minDistance)
-                index = ic;
+            var maxDistance = float.MaxValue;
+            var mouse = Event.current.mousePosition;
+            var t = from.transform;
+            for (var i = 0; i < vertices.Length; i++)
+            {
+                var vertex = vertices[i];
+                var point = HandleUtility.WorldToGUIPoint(t.TransformPoint(vertex));
+                var distance = Vector2.Distance(mouse, point);
+                if (distance < maxDistance)
+                    (index, maxDistance) = (i, distance);
+            }
         }
 
     }
