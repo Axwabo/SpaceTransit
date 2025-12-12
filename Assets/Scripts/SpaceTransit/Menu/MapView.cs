a01f56052b6f35af87fcd3d96402d777eaae340c
Axwabo <axwabo@gmail.com>
2025-12-12 14:14:00 +0100

    move map with pointer

--- Assets/Scripts/SpaceTransit/Menu/MapView.cs
+++ Assets/Scripts/SpaceTransit/Menu/MapView.cs
@@ -1,11 +1,15 @@
 ﻿using SpaceTransit.Routes;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace SpaceTransit.Menu
 {
 
+    [RequireComponent(typeof(RectTransform))]
     public sealed class MapView : MonoBehaviour
     {
+
+        private static Vector2 Mouse => InputSystem.actions["Point"].ReadValue<Vector2>();
 
         [SerializeField]
         private MapStation prefab;
@@ -19,13 +23,28 @@ namespace SpaceTransit.Menu
         [SerializeField]
         private float scale = 1;
 
+        [SerializeField]
+        private float sensitivity = 1;
+
         private bool _placed;
+
+        private RectTransform _this;
+
+        private Vector2 _previousPoint;
+
+        private void Awake() => _this = (RectTransform) transform;
+
+        private void OnEnable()
+        {
+            _this.anchoredPosition = Vector2.zero;
+            _previousPoint = Mouse;
+        }
 
         private void Start()
         {
             if (_placed)
                 return;
-            var parent = transform;
+            var parent = _this;
             foreach (var station in Station.LoadedStations)
             {
                 var t = new GameObject(station.Name).transform;
@@ -34,6 +53,21 @@ namespace SpaceTransit.Menu
                 t.localPosition = new Vector3(stationPosition.x * scale, stationPosition.z * scale);
                 Instantiate(prefab, parent, false).Apply(station, t);
             }
+        }
+
+        private void Update()
+        {
+            var point = Mouse;
+            if (!InputSystem.actions["Click"].IsPressed())
+            {
+                _previousPoint = Mouse;
+                return;
+            }
+
+            if (point == _previousPoint)
+                return;
+            _this.anchoredPosition += (point - _previousPoint) * sensitivity;
+            _previousPoint = point;
         }
 
     }
