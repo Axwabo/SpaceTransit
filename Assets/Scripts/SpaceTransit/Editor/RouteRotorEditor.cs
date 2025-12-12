f3d6c3d2269d36233f1b163bcef205c3d5b21c65
Axwabo <axwabo@gmail.com>
2025-12-12 20:18:24 +0100

    attempt RouteRotorEditor

---
+++ Assets/Scripts/SpaceTransit/Editor/RouteRotorEditor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SpaceTransit.Vaulter;
+using UnityEditor;
+
+namespace SpaceTransit.Editor
+{
+
+    [CustomEditor(typeof(RouteRotor))]
+    public sealed class RouteRotorEditor : UnityEditor.Editor
+    {
+
+        public override void OnInspectorGUI()
+        {
+            var rotor = (RouteRotor) target;
+            var last = TimeSpan.Zero;
+            foreach (var descriptor in rotor.routes)
+            {
+                if (!descriptor)
+                    continue;
+                if (descriptor.Origin.Departure < last)
+                {
+                    EditorGUILayout.HelpBox("Routes are not sequential!", MessageType.Error);
+                    break;
+                }
+
+                last = descriptor.Destination.Arrival.Value;
+            }
+
+            base.OnInspectorGUI();
+        }
+
+    }
+
+}
