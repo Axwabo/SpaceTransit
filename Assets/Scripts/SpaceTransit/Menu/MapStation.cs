ba6651eca5b1dd4c9aa91e852cd5953289e0b1b2
Axwabo <axwabo@gmail.com>
2025-12-12 13:18:47 +0100

    attempt map view

---
+++ Assets/Scripts/SpaceTransit/Menu/MapStation.cs
@@ -0,0 +1,30 @@
+﻿using SpaceTransit.Routes;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Animations;
+
+namespace SpaceTransit.Menu
+{
+
+    public sealed class MapStation : MonoBehaviour
+    {
+
+        [SerializeField]
+        private TextMeshProUGUI text;
+
+        [SerializeField]
+        private PositionConstraint constraint;
+
+        public void Apply(Station station, Transform anchor)
+        {
+            text.text = station.Name;
+            constraint.AddSource(new ConstraintSource
+            {
+                sourceTransform = anchor,
+                weight = 1
+            });
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/SpaceTransit/Menu/MapView.cs
@@ -0,0 +1,35 @@
+﻿using SpaceTransit.Routes;
+using UnityEngine;
+
+namespace SpaceTransit.Menu
+{
+
+    public sealed class MapView : MonoBehaviour
+    {
+
+        [SerializeField]
+        private MapStation prefab;
+
+        [SerializeField]
+        private RectTransform anchor;
+
+        private bool _placed;
+
+        private void Start()
+        {
+            if (_placed)
+                return;
+            var parent = transform;
+            foreach (var station in Station.LoadedStations)
+            {
+                var t = new GameObject(station.Name).transform;
+                var stationPosition = station.transform.localPosition;
+                t.parent = anchor;
+                t.localPosition = new Vector3(stationPosition.x * World.MetersToWorld, stationPosition.z * World.MetersToWorld);
+                Instantiate(prefab, parent).Apply(station, t);
+            }
+        }
+
+    }
+
+}
