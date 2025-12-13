24acff1bac5ac7161520d4f427170e70e4ea7655
Axwabo <axwabo@gmail.com>
2025-12-13 10:10:53 +0100

    modify Peshtadub-East platform + fix station colliders in build

--- Assets/Scripts/SpaceTransit/Build/SceneProcessor.cs
+++ Assets/Scripts/SpaceTransit/Build/SceneProcessor.cs
@@ -9,7 +9,7 @@ using UnityEngine.SceneManagement;
 namespace SpaceTransit.Build
 {
 
-    public sealed class GradualTubeLoaderProcessor : IProcessSceneWithReport
+    public sealed class SceneProcessor : IProcessSceneWithReport
     {
 
         public int callbackOrder => 0;
@@ -20,35 +20,48 @@ namespace SpaceTransit.Build
             if (!TryFindProgress(rootGameObjects, out var progress))
                 return;
             var splines = new List<Spline>();
+            var hoist = new List<HoistColliders>();
             var activates = new List<Activate>();
             var load = new List<GameObject>();
             var activate = new List<GameObject>();
+            var hoistTransforms = new List<MeshCollider>();
             foreach (var root in rootGameObjects)
-            {
-                root.GetComponentsInChildren(splines);
-                root.GetComponentsInChildren(activates);
-                foreach (var spline in splines)
-                {
-                    var go = spline.gameObject;
-                    load.Add(go);
-                    go.SetActive(false);
-                }
-
-                foreach (var instance in activates)
-                {
-                    var go = instance.gameObject;
-                    activate.Add(go);
-                    go.SetActive(false);
-                    Object.DestroyImmediate(instance);
-                }
-            }
-
+                ProcessRoot(root, splines, activates, load, activate, hoist, hoistTransforms);
             if (activate.Count == 0)
                 return;
             var loader = rootGameObjects[0].AddComponent<GradualTubeLoader>();
             loader.Load = load.ToArray();
             loader.Activate = activate.ToArray();
             loader.ProgressContainer = progress;
+        }
+
+        private static void ProcessRoot(GameObject root, List<Spline> splines, List<Activate> activates, List<GameObject> load, List<GameObject> activate, List<HoistColliders> hoist, List<MeshCollider> hoistTransforms)
+        {
+            root.GetComponentsInChildren(splines);
+            root.GetComponentsInChildren(activates);
+            root.GetComponentsInChildren(hoist);
+            foreach (var spline in splines)
+            {
+                var go = spline.gameObject;
+                load.Add(go);
+                go.SetActive(false);
+            }
+
+            foreach (var instance in hoist)
+            {
+                var parent = instance.transform.parent;
+                instance.GetComponentsInChildren(hoistTransforms);
+                foreach (var collider in hoistTransforms)
+                    collider.transform.parent = parent;
+            }
+
+            foreach (var instance in activates)
+            {
+                var go = instance.gameObject;
+                activate.Add(go);
+                go.SetActive(false);
+                Object.DestroyImmediate(instance);
+            }
         }
 
         private static bool TryFindProgress(GameObject[] rootGameObjects, out GameObject progress)
