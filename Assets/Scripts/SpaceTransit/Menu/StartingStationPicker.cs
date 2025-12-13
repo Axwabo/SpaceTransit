6a8c3cc87a1a7a353385b97fc3c50c8ed45a8aa3
Axwabo <axwabo@gmail.com>
2025-12-13 09:50:13 +0100

    fix StartingStationPicker

--- Assets/Scripts/SpaceTransit/Menu/StartingStationPicker.cs
+++ Assets/Scripts/SpaceTransit/Menu/StartingStationPicker.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using SpaceTransit.Movement;
+using SpaceTransit.Routes;
 using TMPro;
 using UnityEngine;
 using Cache = SpaceTransit.Vaulter.Cache;
@@ -15,12 +16,20 @@ namespace SpaceTransit.Menu
         [SerializeField]
         private string defaultValue;
 
+        private readonly List<StationId> _stations = new();
+
         private void Start()
         {
+            _stations.Clear();
             var list = new List<string>();
             foreach (var id in Cache.Stations)
+            {
+                _stations.Add(id);
                 list.Add(id.name);
+            }
+
             list.Sort(StringComparer.OrdinalIgnoreCase);
+            _stations.Sort((a, b) => StringComparer.OrdinalIgnoreCase.Compare(a.name, b.name));
             var index = list.IndexOf(defaultValue);
             var dropdown = GetComponent<TMP_Dropdown>();
             dropdown.AddOptions(list);
@@ -29,7 +38,7 @@ namespace SpaceTransit.Menu
             Change(index);
         }
 
-        private static void Change(int arg0) => MovementController.StartingStation = Cache.Stations[arg0];
+        private void Change(int arg0) => MovementController.StartingStation = _stations[arg0];
 
     }
 
