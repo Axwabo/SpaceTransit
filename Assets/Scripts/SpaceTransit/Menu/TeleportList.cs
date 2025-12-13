f8cd7700fe2983668e067b7a327e98006d23e59c
Axwabo <axwabo@gmail.com>
2025-12-13 09:30:19 +0100

    Revert "Y teleport offset"
    This reverts commit c5f2f1a6bcf8abe2ae04992cb2f793294cefd6c1.

--- Assets/Scripts/SpaceTransit/Menu/TeleportList.cs
+++ Assets/Scripts/SpaceTransit/Menu/TeleportList.cs
@@ -36,7 +36,7 @@ namespace SpaceTransit.Menu
             private void Click()
             {
                 if (!MovementController.Current.Mount)
-                    MovementController.Current.Teleport(Station.Spawnpoint + Vector3.up * 0.1f);
+                    MovementController.Current.Teleport(Station.Spawnpoint);
             }
 
         }
