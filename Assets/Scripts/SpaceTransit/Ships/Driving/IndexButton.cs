915cc813d8fc8c0c9dce8be453818d5d9efbc85f
Axwabo <axwabo@gmail.com>
2025-12-14 23:24:53 +0100

    IndexButton

---
+++ Assets/Scripts/SpaceTransit/Ships/Driving/IndexButton.cs
@@ -0,0 +1,22 @@
+﻿using SpaceTransit.Interactions;
+using SpaceTransit.Ships.Driving.Screens;
+using SpaceTransit.Ships.Modules;
+using UnityEngine;
+
+namespace SpaceTransit.Ships.Driving
+{
+
+    public sealed class IndexButton : ModuleComponentBase, IInteractable
+    {
+
+        [SerializeField]
+        private ScreenSlot slot;
+
+        [SerializeField]
+        private int index;
+
+        public void OnInteracted() => slot.Select(index);
+
+    }
+
+}
