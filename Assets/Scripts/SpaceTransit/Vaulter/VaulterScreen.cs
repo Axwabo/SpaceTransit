c65237912df32765d7d6cdc0671b78ad4700268f
Axwabo <axwabo@gmail.com>
2025-12-14 21:03:04 +0100

    begin screens refactor

---
+++ Assets/Scripts/SpaceTransit/Vaulter/VaulterScreen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace SpaceTransit.Vaulter
+{
+
+    public sealed class VaulterScreen : VaulterComponentBase
+    {
+
+        [SerializeField]
+        private GameObject notInService;
+
+        public override void OnRouteChanged()
+        {
+            base.OnRouteChanged();
+        }
+
+    }
+
+}
