52fe2313106599f1c07008cf218daf1eada1d1cf
Axwabo <axwabo@gmail.com>
2025-12-15 00:55:44 +0100

    tutorial

---
+++ Assets/Scripts/SpaceTransit/Menu/Main/NextImage.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SpaceTransit.Menu.Main
+{
+
+    public sealed class NextImage : AutoRegisterButton
+    {
+
+        [SerializeField]
+        private Image image;
+
+        [SerializeField]
+        private Sprite[] sprites;
+
+        private int _index = -1;
+
+        private void Start() => Click();
+
+        protected override void Click()
+        {
+            if (++_index >= sprites.Length)
+                _index = 0;
+            image.sprite = sprites[_index];
+        }
+
+    }
+
+}
