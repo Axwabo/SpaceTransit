50ea5f4ac971265d4f95045cc5e637caaf1b2767
Axwabo <axwabo@gmail.com>
2026-02-19 10:55:18 +0100

    enter spawn + fix initial start

--- Assets/Scripts/SpaceTransit/Routes/Sequences/RouteManager.cs
+++ Assets/Scripts/SpaceTransit/Routes/Sequences/RouteManager.cs
@@ -39,18 +39,54 @@ namespace SpaceTransit.Routes.Sequences
                 for (var stopIndex = 0; stopIndex < route.IntermediateStops.Length; stopIndex++)
                 {
                     var stop = route.IntermediateStops[stopIndex];
-                    if (!stop.Station.IsLoaded())
+                    if (!Station.TryGetLoadedStation(stop.Station, out var station))
                         return None;
-                    if (stop.Arrival <= Clock.Now && stop.Departure > Clock.Now + TimeSpan.FromMinutes(stop.MinStayMinutes + 1))
+                    if (stop.Departure < Clock.Now + TimeSpan.FromMinutes(stop.MinStayMinutes + 1))
+                        continue;
+                    if (stop.Arrival <= Clock.Now)
                         return (new DockSpawn(stop.DockIndex, stopIndex), routeIndex);
+                    if (stop.Arrival >= Clock.Now + TimeSpan.FromMinutes(1))
+                        return Enter(station, stop, route, stopIndex, routeIndex);
                 }
             }
 
             return None;
         }
 
+        private static (SpawnLocation, int) Enter(Station station, IntermediateStop stop, RouteDescriptor route, int stopIndex, int routeIndex)
+        {
+            var dock = station.Docks[stop.DockIndex];
+            var entries = route.Reverse ? dock.FrontEntries : dock.BackEntries;
+            if (entries.Length == 0)
+            {
+                var next = dock.Next(!route.Reverse);
+                if (!next)
+                    return (new DockSpawn(stop.DockIndex, stopIndex), routeIndex);
+                var second = dock.Next(!route.Reverse);
+                return (new TubeSpawn(second ? second : next, stopIndex), routeIndex);
+            }
+
+            var finalEntry = entries[0];
+            foreach (var entry in entries)
+            {
+                if (entry.Connected != stop.ArriveFrom)
+                    continue;
+                finalEntry = entry;
+                break;
+            }
+
+            if (!finalEntry.IsFree || !finalEntry.Ensurer)
+                return None;
+            var tube = finalEntry.Ensurer.Tube.Next;
+            var finalTube = route.Reverse ? tube.Next : tube;
+            return finalTube
+                ? (new EntrySpawn(tube, finalEntry, stopIndex), routeIndex)
+                : (new DockSpawn(stop.DockIndex, stopIndex), routeIndex);
+        }
+
         private static async Awaitable Start(ServiceSequence sequence, CancellationToken token)
         {
+            await Awaitable.NextFrameAsync(token);
             while (!token.IsCancellationRequested)
             {
                 try
@@ -70,6 +106,12 @@ namespace SpaceTransit.Routes.Sequences
         {
             var ship = Spawn(sequence, spawn, index);
             var token = ship.destroyCancellationToken;
+            if (spawn is TubeSpawn)
+            {
+                await Awaitable.NextFrameAsync(token);
+                ship.Parent.MarkReady();
+            }
+
             while (!token.IsCancellationRequested)
             {
                 await Awaitable.WaitForSecondsAsync(5, token);
