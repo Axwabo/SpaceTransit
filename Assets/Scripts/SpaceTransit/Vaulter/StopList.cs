367b78d375e9c4b32852f59f1518eacb582ee5d0
Axwabo <axwabo@gmail.com>
2026-06-16 11:41:14 +0200

    refactor stop list

--- Assets/Scripts/SpaceTransit/Vaulter/StopList.cs
+++ Assets/Scripts/SpaceTransit/Vaulter/StopList.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using SpaceTransit.Routes.Stops;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace SpaceTransit.Vaulter
@@ -8,9 +9,12 @@ namespace SpaceTransit.Vaulter
     public sealed class StopList : VaulterComponentBase
     {
 
+        [SerializeField]
+        private VisualTreeAsset prefab;
+
         private readonly List<Stop> _stops = new();
 
-        private MultiColumnListView _list;
+        private ListView _list;
 
         private void OnEnable()
         {
@@ -20,16 +24,43 @@ namespace SpaceTransit.Vaulter
 
         protected override void OnInitialized()
         {
-            _list = this.RootVisual().Q<MultiColumnListView>("Stops");
+            _list = this.RootVisual().Q<ListView>("Stops");
             _list.itemsSource = _stops;
+            _list.makeItem = () =>
+            {
+                var element = new VisualElement();
+                prefab.CloneTree(element);
+                return element;
+            };
+            _list.bindItem = (element, i) =>
+            {
+                var stop = _stops[i];
+                element.Q<Label>("Station").text = stop.Station.name;
+                element.Q<Label>("Arrival").text = (stop as IArrival)?.Arrival.ToString() ?? "";
+                element.Q<Label>("Departure").text = (stop as IDeparture)?.Departure.ToString() ?? "";
+                element.Q<Label>("DockIndex").text = (stop.DockIndex + 1).ToString();
+            };
         }
 
         public override void OnRouteChanged()
         {
+            RefreshStops();
+            _list.RefreshItems();
+        }
+
+        public override void OnStopChanged()
+        {
+            if (_stops.Count == 0)
+                return;
+            _stops.RemoveAt(0);
+            _list.RefreshItems();
+        }
+
+        private void RefreshStops()
+        {
             if (!IsInService)
             {
                 _stops.Clear();
-                _list.RefreshItems();
                 return;
             }
 
@@ -39,14 +70,6 @@ namespace SpaceTransit.Vaulter
             foreach (var stop in Parent.NextIntermediateStops)
                 _stops.Add(stop);
             _stops.Add(Parent.Route.Destination);
-        }
-
-        public override void OnStopChanged()
-        {
-            if (_stops.Count == 0)
-                return;
-            _stops.RemoveAt(0);
-            _list.RefreshItems();
         }
 
         public void Navigate(bool forwards)
