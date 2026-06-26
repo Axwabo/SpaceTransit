8f9302064a7761b5a82c7c0ddfdf50ce9db1ba71
Axwabo <axwabo@gmail.com>
2026-06-26 11:21:34 +0200

    sort routes

--- Assets/Scripts/SpaceTransit/Ships/Driving/Screens/RouteListManager.cs
+++ Assets/Scripts/SpaceTransit/Ships/Driving/Screens/RouteListManager.cs
@@ -25,13 +25,16 @@ namespace SpaceTransit.Ships.Driving.Screens
         public void Refresh(TubeBase tube)
         {
             Screen.Source.Clear();
-            Screen.Source.Add(RoutePicker.ExitService);
             if (tube is Dock dock)
             {
                 CacheExits(dock.Station);
-                Append(World.ExtraRoutes.Length != 0 ? World.ExtraRoutes : Cache.Routes, dock);
+                if (World.ExtraRoutes.Length != 0)
+                    Append(World.ExtraRoutes, dock, false);
+                else
+                    Append(Cache.Routes, dock, true);
             }
 
+            Screen.Source.Insert(0, RoutePicker.ExitService);
             Screen.Refresh();
         }
 
@@ -47,11 +50,18 @@ namespace SpaceTransit.Ships.Driving.Screens
             }
         }
 
-        private void Append(ReadOnlySpan<RouteDescriptor> routes, Dock dock)
+        private void Append(ReadOnlySpan<RouteDescriptor> routes, Dock dock, bool sort)
         {
             foreach (var route in routes)
                 if (route.Origin.Station == dock.Station.ID && (AvailableExits.Count == 0 || AvailableExits.Contains(route.Origin.ExitTowards)))
                     Screen.Source.Add(new RoutePicker(route));
+            if (sort)
+                Screen.Source.Sort((a, b) =>
+                {
+                    int.TryParse(a.Descriptor.name, out var na);
+                    int.TryParse(b.Descriptor.name, out var nb);
+                    return na.CompareTo(nb);
+                });
         }
 
         public override void OnStateChanged()
