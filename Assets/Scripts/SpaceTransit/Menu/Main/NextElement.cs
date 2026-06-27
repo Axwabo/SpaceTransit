934e6f9073f6d3ea364b255530c50b1800360159
Axwabo <axwabo@gmail.com>
2026-06-27 14:59:06 +0200

    begin new tutorial

---
+++ Assets/Scripts/SpaceTransit/Menu/Main/NextElement.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using UnityEngine.UIElements;
+
+namespace SpaceTransit.Menu.Main
+{
+
+    public sealed class NextElement : AutoRegisterButton
+    {
+
+        private VisualElement _container;
+
+        private VisualElement _previous;
+
+        private int _index = -1;
+
+        protected override void Start()
+        {
+            base.Start();
+            _container = this.RootVisual().Q<VisualElement>("Container");
+            Click();
+        }
+
+        protected override void Click()
+        {
+            if (++_index >= _container.childCount)
+                _index = 0;
+            _previous?.RemoveFromClassList("active");
+            _previous = _container.Children().ElementAt(_index);
+            _previous.AddToClassList("active");
+        }
+
+    }
+
+}
