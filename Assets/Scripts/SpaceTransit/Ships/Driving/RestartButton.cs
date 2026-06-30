51bf3edd51e5dd3d01756b4f527dae0681acc8fa
Axwabo <axwabo@gmail.com>
2026-06-30 09:45:02 +0200

    don't allow restart at intermediate stops

--- Assets/Scripts/SpaceTransit/Ships/Driving/RestartButton.cs
+++ Assets/Scripts/SpaceTransit/Ships/Driving/RestartButton.cs
@@ -1,4 +1,5 @@
 using SpaceTransit.Interactions;
+using SpaceTransit.Routes.Stops;
 using SpaceTransit.Ships.Modules;
 
 namespace SpaceTransit.Ships.Driving
@@ -7,9 +8,11 @@ namespace SpaceTransit.Ships.Driving
     public sealed class RestartButton : ModuleComponentBase, IInteractable
     {
 
+        private bool VaulterAllowsRestart => !Controller.TryGetVaulter(out var vaulter) || !vaulter.IsInService || vaulter.Target is Origin or Destination;
+
         public void OnInteracted()
         {
-            if (!Controller.IsRestarting)
+            if (!Controller.IsRestarting && Controller.State == ShipState.Docked && VaulterAllowsRestart)
                 _ = Controller.RestartAsync();
         }
 
