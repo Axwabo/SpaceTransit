7fa23c28abc3781858a915b5e395be13fa1357f4
Axwabo <axwabo@gmail.com>
2026-07-11 16:50:36 +0200

    begin onboard display refactor

--- Assets/Scripts/UIElementExtensions.cs
+++ Assets/Scripts/UIElementExtensions.cs
@@ -1,11 +1,16 @@
 using UnityEditor;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 public static class UIElementExtensions
 {
 
     [InitializeOnLoadMethod]
-    private static void Init() => ConverterGroups.RegisterGlobalConverter<bool, StyleEnum<DisplayStyle>>((ref bool display) => display ? DisplayStyle.Flex : DisplayStyle.None);
+    private static void Init()
+    {
+        ConverterGroups.RegisterGlobalConverter<bool, StyleEnum<DisplayStyle>>((ref bool display) => display ? DisplayStyle.Flex : DisplayStyle.None);
+        ConverterGroups.RegisterGlobalConverter<Color, StyleColor>((ref Color color) => color);
+    }
 
     public static void SetVisibility(this VisualElement element, bool visible)
         => element.style.display = visible ? DisplayStyle.Flex : DisplayStyle.None;
