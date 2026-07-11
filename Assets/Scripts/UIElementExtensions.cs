7fe8ce889ce983ec73c7610adbba181d2e45ba66
Axwabo <axwabo@gmail.com>
2026-07-11 16:18:26 +0200

    fix display converter

--- Assets/Scripts/UIElementExtensions.cs
+++ Assets/Scripts/UIElementExtensions.cs
@@ -5,7 +5,7 @@ public static class UIElementExtensions
 {
 
     [InitializeOnLoadMethod]
-    private static void Init() => ConverterGroups.RegisterGlobalConverter((ref bool display) => display ? DisplayStyle.Flex : DisplayStyle.None);
+    private static void Init() => ConverterGroups.RegisterGlobalConverter<bool, StyleEnum<DisplayStyle>>((ref bool display) => display ? DisplayStyle.Flex : DisplayStyle.None);
 
     public static void SetVisibility(this VisualElement element, bool visible)
         => element.style.display = visible ? DisplayStyle.Flex : DisplayStyle.None;
