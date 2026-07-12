2b153dc571f48afc5f430e66c36ea35c5275a5e4
Axwabo <axwabo@gmail.com>
2026-07-12 09:47:53 +0200

    attempt RouteList refactor

--- Assets/Scripts/SpaceTransit/Menu/RouteList.cs
+++ Assets/Scripts/SpaceTransit/Menu/RouteList.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using SpaceTransit.Routes;
+using Unity.Properties;
 using UnityEngine;
 using UnityEngine.UIElements;
 using Cache = SpaceTransit.Vaulter.Cache;
@@ -15,31 +15,23 @@ namespace SpaceTransit.Menu
 
         private RouteDescriptor[] _routes;
 
+        [CreateProperty]
+        public string[] Routes { get; set; }
+
+        [CreateProperty]
+        public int SelectedIndex
+        {
+            set => timetable.Apply(_routes[value]);
+        }
+
         [SerializeField]
         private RouteTimetable timetable;
 
         private void Start()
         {
             _routes = Cache.Journeys.OfType<RouteDescriptor>().OrderBy(e => int.Parse(e.name)).ToArray();
-            _list = this.RootVisual().Q<ListView>("Routes");
-            _list.makeItem = () => new Label();
-            _list.bindItem = Bind;
-            _list.itemsSource = _routes;
-            _list.selectionChanged += OnSelectionChanged;
-        }
-
-        private void Bind(VisualElement element, int i)
-        {
-            var route = _routes[i];
-            var label = element as Label ?? element.Q<Label>();
-            label.text = $"{route.name} {route.Beginning.Station.name} - {route.End.Station.name}";
-        }
-
-        private void OnSelectionChanged(IEnumerable<object> objects)
-        {
-            var descriptor = objects.FirstOrDefault();
-            if (descriptor is RouteDescriptor route)
-                timetable.Apply(route);
+            Routes = _routes.Select(e => $"{e.name} {e.Origin.Station.name} - {e.Destination.Station.name}").ToArray();
+            this.RootVisual().dataSource = this;
         }
 
     }
