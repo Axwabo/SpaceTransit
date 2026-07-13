7d9c906528655d04168aa09d52a8831f8803badb
Axwabo <axwabo@gmail.com>
2026-07-13 11:55:28 +0200

    fix UIElementExtensions for build

--- Assets/Scripts/UIElementExtensions.cs
+++ Assets/Scripts/UIElementExtensions.cs
@@ -1,10 +1,16 @@
-using UnityEditor;
+using UnityEngine;
 using UnityEngine.UIElements;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public static class UIElementExtensions
 {
 
+#if UNITY_EDITOR
     [InitializeOnLoadMethod]
+#endif
+    [RuntimeInitializeOnLoadMethod]
     private static void Init() => ConverterGroups.RegisterGlobalConverter<bool, StyleEnum<DisplayStyle>>((ref bool display) => display ? DisplayStyle.Flex : DisplayStyle.None);
 
     public static void SetVisibility(this VisualElement element, bool visible)
